ce48ee6ef639e918e894ddd19c65528e84fd8b20
AndriiKot <a_kocyuba@ukr.net>
2023-03-30 10:46:56 +0200

    class superclass and ancestors

--- classes_for_ruby1.rb
+++ classes_for_ruby1.rb
@@ -8,3 +8,10 @@ end
 
 class Manager < Person
 end
+
+
+manager1 = Manager.new 'Mr. Joe',30
+p manager1                    # #<Manager: ... @name = "Mr. Joe",@age = 30>
+p manager1.class              # Manager
+p manager1.class.superclass   # Person
+p manager1.class.ancestors    # [Manager,Person,Object,Kernel,BasicObject]
