75ad8fc7de97c3ff0ad83dbbbf13a019386abc9d
AndriiKot <a_kocyuba@ukr.net>
2023-03-30 11:21:46 +0200

    Add classese 3 and 4 files

---
+++ classes_for_ruby3.rb
@@ -0,0 +1,19 @@
+﻿class Animal
+end
+
+p Animal.class            # Class
+p Animal.class.superclass # Module
+p Animal.class.ancestors  # [Class,Module,Object,Kernel,BasicObject]
+
+module MyModule
+end
+
+=begin
+class Animal < MyModule     # TypeError
+end
+=end
+
+
+
+
+
---
+++ classes_for_ruby4.rb
@@ -0,0 +1,19 @@
+﻿class Person
+  NAME = 'Anonymous'
+end
+
+name = Person
+
+p name::NAME     # 'Anonymous'        
+
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+oxi = name.new 'Oxi'
+p oxi.name  "Oxi"
+
+ 
